07118eceb7350935c53d2208043b5e95575a657c
ermi9s <ermiayele1122334477@gmail.com>
2023-11-27 15:54:31 +0300

    Week 2 mini project
    Student database

---
+++ Week2.py
@@ -0,0 +1,81 @@
+student = dict()
+
+def add():
+    print("Enter your name")
+    name = input()
+
+    print("Enter your age")
+    age = input()
+
+    print("Enter your grade")
+    grade = input()
+
+    student[name] = [name, age, grade]
+
+def view():
+    print("NAME  AGE  GRADE")
+    for i in student.values():
+            for j in i:
+                print (j,end="  ")
+            print("\n")
+                
+
+def update():
+    print("Enter the name")
+    uname = input()
+    if(student.get(uname, 0)):
+        print("studet found")
+        print("Enter 1 to update name")
+        print("Enter 2 to update age")
+        print("Enter 3 to update grade")
+        c2 = int(input())
+        if c2 == 1:
+            print("Enter name")
+            student[uname][0] = input()
+        elif c2 == 2:
+            print("Enter age")
+            student[uname][1] = input()
+        elif c2 == 3:
+            print("Enter grade")
+            student[uname][2] = input()
+    else:
+        print("Student not found")
+
+def delete():
+    print("Enter the name")
+    dname = input()
+    del student[dname]
+
+control = bool(int(input("Enter 1 to start, 0 to stop: ")))
+
+while(control):
+    print("1, Add\n2, Update\n3, View\n4, Delete a student")
+    cho = int(input())
+
+    if cho == 1:
+        add()
+        control = bool(int(input("Enter 1 to continue, 0 to stop: ")))
+    elif cho == 2: 
+        update()
+        control = bool(int(input("Enter 1 to continue, 0 to stop: ")))
+    elif cho == 3:  
+        print("1, View specific student")
+        print("2, View all students")
+        choi = int(input())  
+        if choi == 1:
+            print("Enter his name")
+            name = input()
+            print("NAME  AGE  GRADE")
+            for i in student[name]:
+                print (i,end="  ")
+            
+            control = bool(int(input("\nEnter 1 to continue, 0 to stop: ")))
+        else:
+            view()
+            control = bool(int(input("\nEnter 1 to continue, 0 to stop: ")))
+    elif cho == 4:
+        delete()
+
+
+    
+
